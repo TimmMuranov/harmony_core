2681cdf7ec197fa1b61cc40f126169372db124a1
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-04-01 19:32:40 +0300

    Update testTgBot.cpp
    По моему, это идеальный вариант

--- testTgBot.cpp
+++ testTgBot.cpp
@@ -1,62 +1,71 @@
 /* 
 g++ testTgBot.cpp -o bin/testTgBot --std=c++20 -I/usr/local/include -lTgBot -lboost_system -lssl -lcrypto -lpthread
 clear
-*/   
-#include <stdio.h>
+*/ 
+#include <iostream>
 #include <tgbot/tgbot.h>
 
-#include "core/core.h"
+using namespace std;
+using namespace TgBot;
 
-TgBot::Bot bot("token");
-string mode = "start";   // костыль, позволяющий определить режим работы бота
-string command = "";     // костыль, хранящий команду режима
-Note note;
+Bot bot("7923105858:AAFPCA1cc1sacxBdUpfQP1aeWI-Rv_mTdFg");
 
-int main(){
-//-------------------------- Установка режима работы бота -----------------------------------
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        mode = "satrt";
-        bot.getApi().sendMessage(message->chat->id, "Доступные режимы работы:\n/note");
-    });
-    bot.getEvents().onCommand("note", [&bot](TgBot::Message::Ptr message){
-        mode = "note";
-        bot.getApi().sendMessage(message->chat->id,
-            "Режим работы с нотой акривирован. Доступные команды:\n"
-            "/set - установить имя и знак ноты\n"
-            "/setoctave - установить октаву ноты\n"
-            "/get - получить имя ноты и ее октаву\n"
-            "/exit - выйти из режима работы с нотой"
-        );
-    });
-    bot.getEvents().onCommand("note", [&bot](TgBot::Message::Ptr message) {
-        
-        bot.getApi().sendMessage(message->chat->id, "Режим ноты активирован");
-    });
-    bot.getEvents().onCommand("interval", [&bot](TgBot::Message::Ptr message){
-        mode = "interval";
-        bot.getApi().sendMessage(message->chat->id, "Режим работы с интервалом активирован");
-    });
-//-------------------- обработчик сообщений в зависимости от режима -------------------------
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        string userText = message->text.c_str();
-        if(userText == "/start" || userText == "/note" || userText == "/interval") return;
+boost::variant< std::int64_t, std::string > workChat; // хранит айди рабочего чата
+Message::Ptr workMessage; // хранит данные сообщения-монитора
+Message::Ptr keyMessage; // хранит данные сообщения-клавиатуры
 
-        printf("User wrote %s\n", userText);
-        
-        bot.getApi().sendMessage(message->chat->id, "Вы ввели: " + userText + ".\nРежим работы: " + mode);
+int main() {
+    bot.getEvents().onCommand("start", [&bot](Message::Ptr message){
+        bot.getApi().sendMessage(message->chat->id, "Прив ^^");
+        workChat = message->chat->id;
     });
-//-----------------------------      старт      -----------------------------------
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
+    bot.getEvents().onCommand("note", [&bot](Message::Ptr message) {
+        workChat = message->chat->id; // сохраняем идентификатор чата
+        InlineKeyboardMarkup::Ptr keyboard(new InlineKeyboardMarkup); // cсоздаем доп. клавиатуру
+
+        InlineKeyboardButton::Ptr button1(new InlineKeyboardButton);  // создаем кнопки
+        button1->text = "Завершить работу";
+        button1->callbackData = "but1";
+
+        InlineKeyboardButton::Ptr button2(new InlineKeyboardButton);
+        button2->text = "Кнопка 2";
+        button2->callbackData = "but2";
+
+        InlineKeyboardButton::Ptr button3(new InlineKeyboardButton);
+        button3->text = "Кнопка 3";
+        button3->callbackData = "but3";
+
+        vector<vector<InlineKeyboardButton::Ptr>> rows;
+        rows.push_back({ button1});   // Первый ряд с одной кнопкой
+        rows.push_back({ button2, button3 });  // Второй ряд с двумя кнопками
+
+        keyboard->inlineKeyboard = rows;
+
+        LinkPreviewOptions::Ptr ptr;
+        workMessage = bot.getApi().sendMessage(message->chat->id, "...");
+        keyMessage = bot.getApi().sendMessage(message->chat->id, "Нажимай на кнопки и изменяй сообщение с данными сверху", ptr, 0, keyboard);
+    });
+
+    bot.getEvents().onCallbackQuery([&bot](CallbackQuery::Ptr query) {
+        string data = query->data;
+        if (data == "but1") {
+            bot.getApi().deleteMessage(workChat, keyMessage->messageId);
+            bot.getApi().sendMessage(workChat, "Работа завершена.");
+        } else if (data == "but2") {
+            bot.getApi().editMessageText("Вы нажали вторую кнопку", workChat, workMessage->messageId);
+        } else if (data == "but3") {
+            bot.getApi().editMessageText("Третья кнопка нажата", workChat, workMessage->messageId);
         }
-    } catch (TgBot::TgException& e) {
-        printf("error: %s\n", e.what());
+    });
+
+    try {
+        TgLongPoll longPoll(bot);
+        while (true) {
+            longPoll.start();
+            cout << "LP started\n";
+        }
+    } catch (exception& e) {
+        cerr << e.what() << endl;
     }
     return 0;
 }
-//Я просто хз как сдвинуться с этой мертвой точки. Эту библиотеку определенно написало существо с другой планеты....
-
